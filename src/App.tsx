50700aaed74ef36451e052ac25990b6c97a901f0
Owais Mansha <awaismansha1998@gmail.com>
2020-10-22 20:39:12 +0500

    pushing

---
+++ src/App.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { unescapeLeadingUnderscores } from 'typescript';
+import { Difficulty, fetchQuestions, QuestionState } from './API';
+import { QuestionCard } from './components/QuestionCard';
+import {GlobalStyle,Wrapper} from './App.styles'
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+function App() {
+
+
+  const [totalQuestions, settotalQuestions] = useState(2)
+  const [loading, setloading] = useState(false)
+  const [questions, setquestions] = useState<QuestionState[]>([])
+  const [number, setnumber] = useState(0)
+  const [userAnswers, setuserAnswers] = useState<AnswerObject[]>([])
+  const [score, setscore] = useState(0)
+  const [gameOver, setgameOver] = useState(true)
+
+  const startQuiz = async () => {
+    setloading(true)
+    setgameOver(false)
+    setquestions(await fetchQuestions(totalQuestions, Difficulty.EASY))
+    setscore(0)
+    setuserAnswers([])
+    setnumber(0)
+    setloading(false)
+
+  }
+  const nextQuestion = async () => {
+    
+    const nextQuestion = number+1;
+    if(nextQuestion===totalQuestions){
+      setgameOver(true)
+    }else{
+      setnumber(nextQuestion)
+    }
+   }
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if(!gameOver){
+      const res= e.currentTarget.value;
+      const check = questions[number].correct_answer=== res;
+      if(check){
+        setscore(prev => prev + 1)
+      }
+      const AnswerObject={
+        question:questions[number].question,
+        answer:res,
+        correct:check,
+        correctAnswer:questions[number].correct_answer
+      }
+      setuserAnswers(prev => [...prev,AnswerObject])
+    }
+   }
+
+  console.log(fetchQuestions(totalQuestions, Difficulty.EASY))
+
+  return (
+    <>
+    <GlobalStyle />
+    <Wrapper>
+   
+      <h1> Quiz App by Owys</h1>
+      {
+        gameOver || userAnswers.length === totalQuestions ? (<button className='start' onClick={startQuiz}>
+          Start Quiz
+        </button>) : null
+      }
+
+
+      {
+        !gameOver ? (<p className='score'>
+          score : {score}</p>) : null
+      }
+      {
+        loading ? (<p>Loading</p>) : null
+      }
+      {!loading && !gameOver ? (<QuestionCard
+        questionNumber={number + 1}
+        totalQuestions={totalQuestions}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      ></QuestionCard>) : null}
+      {
+        !gameOver && !loading && userAnswers.length === number + 1 && number !== totalQuestions - 1 ? (<button className='next' onClick={nextQuestion}>
+          Next Question
+        </button>) : null
+      }
+    </Wrapper>
+</>
+  );
+}
+
+export default App;
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Wrapper,ButtonWrapper} from './QuestionCard.styles'
+
+type Props={
+    question : string;
+    answers: string[];
+    callback: any;
+    userAnswer:any;
+    questionNumber:number;
+    totalQuestions:number;
+}
+
+export const QuestionCard:React.FC<Props> = ({question,answers,callback,
+    userAnswer,questionNumber,totalQuestions}) => {
+    
+    return (
+        <Wrapper>
+            <p> Question : {questionNumber} / {totalQuestions}</p>
+            <p dangerouslySetInnerHTML={{__html:question}}/>
+            <div>
+                {answers.map(answer=>(
+                  <ButtonWrapper
+                  correct={userAnswer?.correctAnswer === answer}
+                  userClicked={userAnswer?.answer===answer}
+                  >
+                      <button value={answer} disabled={userAnswer} onClick={callback}> 
+                          <span dangerouslySetInnerHTML={{__html:answer}}></span>
+                      </button>
+                  </ButtonWrapper>  
+                ))}
+            </div>
+            
+        </Wrapper>
+    )
+}
