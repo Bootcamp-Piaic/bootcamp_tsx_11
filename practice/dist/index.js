ec0670eda173c782608e958702860edf6d12c88b
Owais Mansha <awaismansha1998@gmail.com>
2020-10-22 21:17:54 +0500

    last check

---
+++ practice/dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+console.log("hola");
+// let a =137;
+// let b=5+[1,2,3,4]
+var obg = {
+    foo: 0
+};
+var c = [4, 5, 6, 6];
+c[1] = 6;
+console.log(obg.foo);
+console.log(c[3]);
+var token;
+token = {
+    id: 137,
+    name: 'awais'
+};
+function check(a) {
+    return a * a;
+}
+check(5);
---
+++ practice/src/index.ts
@@ -0,0 +1,26 @@
+
+
+
+console.log("hola")
+
+// let a =137;
+// let b=5+[1,2,3,4]
+let obg={
+    foo : 0
+
+}
+let c:number[]=[4,5,6,6]
+c[1]=6
+console.log(obg.foo)
+console.log(c[3])
+
+let token:{id:number,name:string};
+
+token={
+    id:137,
+    name:'awais'
+}
+function check(a:number){
+    return a*a;
+}
+check(5);
